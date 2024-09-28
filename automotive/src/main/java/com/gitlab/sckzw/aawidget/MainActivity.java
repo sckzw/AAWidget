c2cc16cb45e502e95230e6b0b0a126a52531f598
shuichi <shuichi.kozawa@gmail.com>
2024-09-28 20:10:22 +0900

    Add WidgetListActivity

--- automotive/src/main/java/com/gitlab/sckzw/aawidget/MainActivity.java
+++ automotive/src/main/java/com/gitlab/sckzw/aawidget/MainActivity.java
@@ -2,26 +2,27 @@ package com.gitlab.sckzw.aawidget;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.preference.PreferenceFragmentCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate( Bundle savedInstanceState ) {
         super.onCreate( savedInstanceState );
-        //EdgeToEdge.enable( this );
-        setContentView( R.layout.activity_main );
-        /*
-        ViewCompat.setOnApplyWindowInsetsListener( findViewById( R.id.main ), ( v, insets ) -> {
-            Insets systemBars = insets.getInsets( WindowInsetsCompat.Type.systemBars() );
-            v.setPadding( systemBars.left, systemBars.top, systemBars.right, systemBars.bottom );
-            return insets;
-        } );
 
-         */
+        setContentView( R.layout.activity_main );
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace( R.id.layout_preference, new PreferenceFragment() )
+                .commit();
+    }
+
+    public static class PreferenceFragment extends PreferenceFragmentCompat {
+        @Override
+        public void onCreatePreferences( Bundle savedInstanceState, String rootKey ) {
+            setPreferencesFromResource( R.xml.root_preferences, rootKey );
+        }
     }
 }
---
+++ automotive/src/main/java/com/gitlab/sckzw/aawidget/WidgetListActivity.java
@@ -0,0 +1,18 @@
+package com.gitlab.sckzw.aawidget;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class WidgetListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState ) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_widget_list );
+    }
+}
