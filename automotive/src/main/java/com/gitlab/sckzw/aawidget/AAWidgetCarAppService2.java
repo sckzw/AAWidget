66e0a017fbae289628a81c507c9db7116ce82d6e
shuichi <shuichi.kozawa@gmail.com>
2024-10-16 01:37:13 +0900

    Add POI category service

---
+++ automotive/src/main/java/com/gitlab/sckzw/aawidget/AAWidgetCarAppService2.java
@@ -0,0 +1,32 @@
+package com.gitlab.sckzw.aawidget;
+
+import android.annotation.SuppressLint;
+import android.content.pm.ApplicationInfo;
+
+import androidx.annotation.NonNull;
+import androidx.car.app.CarAppService;
+import androidx.car.app.Session;
+import androidx.car.app.SessionInfo;
+import androidx.car.app.validation.HostValidator;
+
+public class AAWidgetCarAppService2 extends CarAppService {
+    @NonNull
+    @Override
+    public Session onCreateSession( @NonNull SessionInfo sessionInfo ) {
+        return new AAWidgetSession();
+    }
+
+    @SuppressLint("PrivateResource")
+    @NonNull
+    @Override
+    public HostValidator createHostValidator() {
+        if ( ( getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE) != 0 ) {
+            return HostValidator.ALLOW_ALL_HOSTS_VALIDATOR;
+        }
+        else {
+            return new HostValidator.Builder( getApplicationContext() )
+                    .addAllowedHosts( androidx.car.app.R.array.hosts_allowlist_sample )
+                    .build();
+        }
+    }
+}
