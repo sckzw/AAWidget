4740b3429071dd2d0c941dd49b25373a4cd63d6c
shuichi <shuichi.kozawa@gmail.com>
2024-10-27 23:21:43 +0900

    Update version

--- automotive/build.gradle.kts
+++ automotive/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.gitlab.sckzw.aawidget"
         minSdk = 28
         targetSdk = 34
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "0.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
