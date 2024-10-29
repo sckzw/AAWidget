33f44db0b3325b9bf461f8809f9ad641bf271f2c
shuichi <shuichi.kozawa@gmail.com>
2024-10-29 22:12:15 +0900

    Set DarkMode onCreate and onDestroy

--- automotive/src/main/java/com/gitlab/sckzw/aawidget/AAWidgetSession.java
+++ automotive/src/main/java/com/gitlab/sckzw/aawidget/AAWidgetSession.java
@@ -10,15 +10,18 @@ import androidx.car.app.CarContext;
 import androidx.car.app.CarToast;
 import androidx.car.app.Screen;
 import androidx.car.app.Session;
+import androidx.lifecycle.DefaultLifecycleObserver;
+import androidx.lifecycle.LifecycleOwner;
 
 import java.util.Objects;
 
-public class AAWidgetSession extends Session {
+public class AAWidgetSession extends Session implements DefaultLifecycleObserver {
     private final Class< ? extends CarAppService > mCarAppServiceClass;
 
     public AAWidgetSession( Class< ? extends CarAppService > carAppServiceClass ) {
         super();
         mCarAppServiceClass = carAppServiceClass;
+        getLifecycle().addObserver( this );
     }
 
     @NonNull
@@ -28,20 +31,34 @@ public class AAWidgetSession extends Session {
     }
 
     @Override
+    public void onNewIntent( @NonNull Intent intent ) {
+        super.onNewIntent( intent );
+        if ( Objects.equals( intent.getAction(), CarContext.ACTION_NAVIGATE ) ) {
+            CarToast.makeText( getCarContext(), R.string.switch_to_google_maps_to_navigate, CarToast.LENGTH_LONG ).show();
+        }
+    }
+
+    @Override
     public void onCarConfigurationChanged( @NonNull Configuration newConfiguration ) {
+        syncDarkMode();
+    }
+
+    @Override
+    public void onCreate( @NonNull LifecycleOwner owner ) {
+        syncDarkMode();
+    }
+
+    @Override
+    public void onDestroy( @NonNull LifecycleOwner owner ) {
+        AppCompatDelegate.setDefaultNightMode( AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM );
+    }
+
+    private void syncDarkMode() {
         if ( getCarContext().isDarkMode() ) {
             AppCompatDelegate.setDefaultNightMode( AppCompatDelegate.MODE_NIGHT_YES );
         }
         else {
             AppCompatDelegate.setDefaultNightMode( AppCompatDelegate.MODE_NIGHT_NO );
-        }
-    }
-
-    @Override
-    public void onNewIntent( @NonNull Intent intent ) {
-        super.onNewIntent( intent );
-        if ( Objects.equals( intent.getAction(), CarContext.ACTION_NAVIGATE ) ) {
-            CarToast.makeText( getCarContext(), R.string.switch_to_google_maps_to_navigate, CarToast.LENGTH_LONG ).show();
         }
     }
 }
